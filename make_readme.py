645df1157b6eaf3286652e6a8b5d139cd22c00d6
Krystof Litomisky <krystof@litomisky.com>
2020-10-05 21:17:02 -0700

    If there's a new Klog post, tweet about it

--- make_readme.py
+++ make_readme.py
@@ -4,10 +4,12 @@ Build README.md, including the latest blog posts
 '''
 
 import argparse
+import os
 import sys
 import traceback
 import xml.etree.ElementTree as ET
 
+from twython import Twython
 import requests
 
 from eglogging import *
@@ -20,62 +22,119 @@ BLOG_FEED_URL = 'https://krystof.litomisky.com/feed.xml'
 NUM_POSTS = 5
 
 
-def get_blog_posts() -> str:
-  '''
-  Return the markdown of the blog posts
-  '''
-  response = requests.get(BLOG_FEED_URL)
-  content  = response.content
 
-  root    = ET.fromstring(content)
-  channel = root.find('channel')
+class Readme_builder:
 
-  # markdown like [blog post title](url)
-  posts = []
+  def __init__(self, args):
+    # args are from argparse
+    self.args = args
 
-  # add the appropriate number of posts
-  for post in channel.findall('item'):
-    title = post.find('title').text
-    url   = post.find('link' ).text
+    # list of latest Klog posts
+    self.klog_posts = []
 
-    url_with_utm = '{}?utm_source=krystofl_github'.format(url)
-
-    md = '[{}]({})'.format(title, url_with_utm)
-    posts.append(md)
-
-    if len(posts) >= NUM_POSTS:
-      break
-
-  ret = "### Latest Blog Posts\n"
-  for post in posts:
-    ret += "- {}\n".format(post)
-
-  ret += "\nMore at [krystof.litomisky.com]" \
-         "(https://krystof.litomisky.com/?utm_source=krystofl_github)\n"
-
-  return ret
+    # the original README - before this script re-generated it
+    self.README_ORIG = ''
 
 
+  def get_blog_posts(self) -> str:
+    '''
+    Return the markdown of the blog posts
+    '''
+    self.klog_posts = []
 
-def build_readme(args):
-  '''
-  Build README.md
-  '''
-  # get the latest blog posts
+    response = requests.get(BLOG_FEED_URL)
+    content  = response.content
 
-  # load the template
-  with open(args.template, 'r') as fp:
-    TEMPLATE = fp.read()
+    root    = ET.fromstring(content)
+    channel = root.find('channel')
 
-  # add the blog posts
-  BLOG_POSTS_MD = get_blog_posts()
-  result = TEMPLATE.replace("<!-- BLOG POSTS HERE -->", BLOG_POSTS_MD)
+    # markdown like [blog post title](url)
+    posts = []
 
-  # save the result
-  with open(args.output, 'w') as fp:
-    fp.write(result)
+    # add the appropriate number of posts
+    for post in channel.findall('item'):
+      title = post.find('title').text
+      url   = post.find('link' ).text
 
-  return
+      # add it to the list of posts
+      self.klog_posts.append({ "title": title,
+                               "url"  : url })
+
+      url_with_utm = '{}?utm_source=krystofl_github'.format(url)
+
+      md = '[{}]({})'.format(title, url_with_utm)
+      posts.append(md)
+
+      if len(posts) >= NUM_POSTS:
+        break
+
+    ret = "### Latest Blog Posts\n"
+    for post in posts:
+      ret += "- {}\n".format(post)
+
+    ret += "\nMore at [krystof.litomisky.com]" \
+           "(https://krystof.litomisky.com/?utm_source=krystofl_github)\n"
+    return ret
+
+
+  def build_readme(self):
+    '''
+    Build README.md
+    '''
+    # store the original README - useful for detecting changes
+    with open(self.args.output, 'r') as fp:
+      self.README_ORIG = fp.read()
+
+    # load the template
+    with open(self.args.template, 'r') as fp:
+      TEMPLATE = fp.read()
+
+    # add the blog posts
+    BLOG_POSTS_MD = self.get_blog_posts()
+    result = TEMPLATE.replace("<!-- BLOG POSTS HERE -->", BLOG_POSTS_MD)
+
+    # save the result
+    with open(self.args.output, 'w') as fp:
+      fp.write(result)
+    return
+
+
+  def tweet(self):
+    '''
+    Tweets if a new Klog post was found
+    '''
+    # check if we are supposed to tweet
+    if not self.args.yes_tweet:
+      INFO("Not tweeting because --yes_tweet is not set")
+      return
+
+    # make sure we found some Klog posts
+    if len(self.klog_posts) == 0:
+      INFO("No Klog posts found")
+      return
+
+    # check if a new tweet was found
+    with open(self.args.output, 'r') as fp:
+      NEW_README = fp.read()
+    if NEW_README == self.README_ORIG:
+      INFO("No new posts found - not tweeting")
+      return
+
+    # OK - tweet out the latest post
+    latest = self.klog_posts[0]
+    url = "{}?utm_source=krystofs_twitter_bot".format(latest['url'])
+    tweet = "New #blogpost: {}: {}".format(latest['title'], url)
+
+    INFO("Tweet: {}".format(tweet))
+
+    # login to Twitter
+    twitter = Twython(os.environ['TWITTER_API_KEY'],
+                      os.environ['TWITTER_API_SECRET_KEY'],
+                      os.environ['TWITTER_ACCESS_TOKEN'],
+                      os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+    # tweet
+    twitter.update_status(status = tweet)
 
 
 
@@ -93,6 +152,10 @@ def parse_command_line_args():
                       help = 'Template from which to build the output file. ' \
                       'README_TEMPLATE.md by default.')
 
+  parser.add_argument('-y', '--yes_tweet',
+                      action = 'store_true',
+                      help = 'When set, tweet when a new post is found')
+
   args = parser.parse_args()
   return args
 
@@ -103,7 +166,12 @@ if __name__ == '__main__':
     try:
       args   = parse_command_line_args()
 
-      build_readme(args)
+      # build the README
+      builder = Readme_builder(args)
+      builder.build_readme()
+
+      # tweet, but only if appropriate
+      builder.tweet()
 
     except Exception as ex:
       CRITICAL("Exception: {}".format(ex))
