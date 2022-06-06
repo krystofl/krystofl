9dd749fbb6bdf81baa6901d1d071d44a734f411a
Krystof Litomisky <krystof@litomisky.com>
2022-06-05 21:57:44 -0700

    Show a separate list of software posts in the README

--- README_TEMPLATE.md
+++ README_TEMPLATE.md
@@ -5,9 +5,17 @@
 I'm Krystof -
 [engineer, adventurer, and all-around good guy](https://krystof.litomisky.com/about/?utm_source=krystofl_github).
 
-I'm currently thinking about the future and what it needs -
-[let me know if you'd like to build something great](https://krystof.litomisky.com/contact/?utm_source=krystofl_github).
+I improve people's lives through applications of intelligent systems, whether that's robots or software. I've lead teams on both the business and engineering sides of innovative technology companies, including as CEO at Ecogate, where we made the Inc. 5000 list of the fastest-growing private businesses in the US.
+
+I’m a versatile big-picture guy, and I love taking great ideas and turning them into successful products that have a positive impact. I can take charge of everything from software dev to strategy to business negotiations and copywriting while scaling up and down supporting personnel as needed to meet business goals.
+
+[Let's build something great!](https://krystof.litomisky.com/contact/?utm_source=krystofl_github)
+
 
 <!-- BLOG POSTS HERE -->
+
+<!-- SW BLOG POSTS HERE -->
+
+More at [krystof.litomisky.com](https://krystof.litomisky.com/?utm_source=krystofl_github)
 
 Make it a great day!
--- make_readme.py
+++ make_readme.py
@@ -9,6 +9,8 @@ import sys
 import traceback
 import xml.etree.ElementTree as ET
 
+from html.parser import HTMLParser
+
 from twython import Twython
 import requests
 
@@ -16,10 +18,113 @@ from eglogging import *
 logging_load_human_config()
 
 
+BLOG_BASE_URL = 'https://krystof.litomisky.com'
 BLOG_FEED_URL = 'https://krystof.litomisky.com/feed.xml'
+BLOG_TAGS_URL = 'https://krystof.litomisky.com/tags/'
 
-# number of blog posts to include in the list
+# number of blog posts to include in the most-recent list
 NUM_POSTS = 5
+NUM_SW_POSTS = 4
+
+
+class Blog_post():
+  def __init__(self):
+    self.title = ''
+    self.tags  = []        # list of strings
+    self.url   = ''
+    self.publish_date = '' # string as it appears in the html
+
+
+class Tags_html_parser(HTMLParser):
+
+  def __init__(self):
+    # initialize the base HTMLParser class
+    super().__init__()
+
+    self.posts_by_tag = {} # tag (string) --> list of Blog_posts
+
+    # used for parsing
+    self.current_tag = '' # tag that we're currently parsing
+    self.read_tag    = False # when true, read the next tag name
+
+    self.current_post = Blog_post() # the post we're parsing now
+    self.read_post    = False # when true, we're reading a post now
+    self.read_time    = False # when true, we're reading the publish time now
+    self.read_post_title = False # when true, we're reading the post title
+
+
+  def get_posts_by_tag(self):
+    return self.posts_by_tag
+
+
+  def print(self):
+    '''
+    print the posts we have to stdout
+    '''
+    for tag in self.posts_by_tag:
+      print(f"{tag}:")
+      for post in self.posts_by_tag[tag]:
+        print(f"  {post.title}")
+
+
+  def handle_starttag(self, tag, attrs):
+    # handle h2s - get the name of the tag we're about to parse
+    if tag == 'h2':
+      self.read_tag = True
+
+    # handle lis - each a post
+    if tag == 'li':
+      # INFO("Start tag:", tag)
+      # for attr in attrs:
+      #   print("     attr:", attr)
+      self.current_post = Blog_post()
+      self.read_post = True
+
+    # if we're now reading a post line...
+    if self.read_post:
+      # handle as - links to blog post
+      if tag == 'a':
+        for attr in attrs:
+          if attr[0] == 'href':
+            self.current_post.url = BLOG_BASE_URL + attr[1]
+
+        # note to read the title next
+        self.read_post_title = True
+
+      # read the publish time?
+      if tag == 'time':
+        self.read_time = True
+
+
+  def handle_endtag(self, tag):
+    # if we're now reading a post...
+    if self.read_post:
+      # ...and we got to the closing </li>
+      if tag == 'li':
+        # save the post
+        self.posts_by_tag[self.current_tag].append(self.current_post)
+        self.current_post = Blog_post()
+
+
+  def handle_data(self, data):
+    # are we trying to start reading a new tag?
+    if self.read_tag:
+      self.current_tag = data
+      self.read_tag = False
+      self.posts_by_tag[self.current_tag] = []
+      # INFO(f"current tag: {self.current_tag}", color = LOG_COLORS['GREEN'])
+      return
+
+    # are we reading the publish time of a post?
+    if self.read_time:
+      self.current_post.publish_date = data
+      self.read_time = False
+      return
+
+    # read the title of a post
+    if self.read_post_title:
+      self.current_post.title = data
+      self.read_post_title = False
 
 
 
@@ -36,9 +141,41 @@ class Readme_builder:
     self.README_ORIG = ''
 
 
+  def get_blog_posts_by_tag(self) -> str:
+    try:
+      response  = requests.get(BLOG_TAGS_URL)
+      tags_page = str(response.content)
+      # simplify it - only keep the section I need using string search before starting html processing
+      #   <!-- .entry-header -->
+      #   <div class="entry-content">
+      #     "<!-- Archive by tag -->"
+      #     # what I want is here
+      #   </div>
+      #   ends with <!-- .entry-content -->
+      # so chop off everything up to Archive by tag
+      # and chop from (and including) everything past the last </div>
+      archive_by_tag_start = tags_page.find("<!-- Archive by tag -->")
+      entry_content_end    = tags_page.find("<!-- .entry-content -->")
+      ending_div = tags_page.rfind('</div>', archive_by_tag_start, entry_content_end)
+      archive_by_tag_html = tags_page[archive_by_tag_start : ending_div]
+
+      # with open('archive_by_tag.html', 'w') as fp:
+      #   fp.write(archive_by_tag_html)
+
+      # now parse it!
+      parser = Tags_html_parser()
+      parser.feed(archive_by_tag_html)
+      # parser.print()
+      return parser.get_posts_by_tag()
+
+    except Exception as e:
+      ERROR(e)
+      traceback.print_exc()
+
+
   def get_blog_posts(self) -> str:
     '''
-    Return the markdown of the blog posts
+    Return the markdown of the most recent blog posts
     '''
     self.klog_posts = []
 
@@ -53,6 +190,7 @@ class Readme_builder:
 
     # add the appropriate number of posts
     for post in channel.findall('item'):
+
       title = post.find('title').text
       url   = post.find('link' ).text
 
@@ -71,9 +209,6 @@ class Readme_builder:
     ret = "### Latest Blog Posts\n"
     for post in posts:
       ret += "- {}\n".format(post)
-
-    ret += "\nMore at [krystof.litomisky.com]" \
-           "(https://krystof.litomisky.com/?utm_source=krystofl_github)\n"
     return ret
 
 
@@ -93,10 +228,31 @@ class Readme_builder:
     BLOG_POSTS_MD = self.get_blog_posts()
     result = TEMPLATE.replace("<!-- BLOG POSTS HERE -->", BLOG_POSTS_MD)
 
+    # add the latest software posts
+    SW_POSTS_MD = self.get_sw_posts_md()
+    result = result.replace("<!-- SW BLOG POSTS HERE -->", SW_POSTS_MD)
+
     # save the result
     with open(self.args.output, 'w') as fp:
       fp.write(result)
     return
+
+
+  def get_sw_posts_md(self) -> str:
+    '''
+    Get the markdown string for the latest software posts
+    '''
+    md = '### Latest Software Posts\n' # the return markdown string
+
+    posts_by_tag = self.get_blog_posts_by_tag()
+
+    sw_posts = posts_by_tag['Software']
+    for i in range(NUM_SW_POSTS):
+      p = sw_posts[i]
+      s = f"[{p.title}]({p.url}?utm_source=krystofl_github)"
+      md += f"- {s}\n"
+
+    return md
 
 
   def tweet(self):
@@ -123,7 +279,7 @@ class Readme_builder:
     # OK - tweet out the latest post
     latest = self.klog_posts[0]
     url = "{}?utm_source=krystofs_twitter_bot".format(latest['url'])
-    tweet = "New #blogpost: {}: {}".format(latest['title'], url)
+    tweet = "New blog post: {}: {}".format(latest['title'], url)
 
     INFO("Tweet: {}".format(tweet))
 
